f394514eb9a5e1beb391d4c7a755cb3c06d2fbd2
Nathan Parmar <nathparmar@icloud.com>
2023-10-24 16:23:09 +0100

    last commit

--- Airline_project/src/main/java/Runner.java
+++ Airline_project/src/main/java/Runner.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 public class Runner {
 
+   static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+
 
 
         ArrayList<String> destinations = new ArrayList<>();
@@ -17,22 +19,57 @@ public class Runner {
         destinations.add("Liverpool");
 
         System.out.println("Where would you like to go ?");
-        for( String location : destinations){
+        for (String location : destinations) {
             System.out.println(location);
         }
 
+
+
         String input = scanner.nextLine();
-        System.out.println( "You would like to go to " + input + " ?  (true/false) " );
-        Boolean input2 = scanner.nextBoolean();
+        System.out.println("You would like to go to " + input + " ?  (true/false) ");
+        boolean input2 = scanner.nextBoolean();
 
-        
+        if (input2 == true) {
+            System.out.println("Great!");
+        } else {
+            System.out.println("Oh no! Please have a look at the other options...");
+            destinations.remove(input);
+            System.out.println(destinations);
+        }
+
+//        boolean input2 = scanner.nextBoolean();
+//
+//
+//        switch (input2 == false) {
+//
+//            System.out.println("Oh no! Please have a look at the other options...");
+//            destinations.remove(input);
+//            System.out.println(destinations);
+//            System.out.println("Where would you like to go?");
+//            String input3 = scanner.nextLine();
+//            options();
+//            break;
+
+
+//            if (input2 == true){
+//                break;
+//            }
 
 
 
-
+//
+//        System.out.println("Great!");
 
 
     }
+
+//    public static void options(){
+//        String input = scanner.nextLine();
+//        System.out.println("You would like to go to " + input + " ?  (true/false) ");
+//
+//    }
+
+}
 
 
 
@@ -48,7 +85,7 @@ public class Runner {
 
 
 
-    }
+
 
 
 
